b9228e1bd432b33a76ea958915a9c07f100ac848
Joytaylor <jtaylor2@imsa.edu>
2020-07-10 22:43:18 -0500

    list made

---
+++ src/views/Ingrediants.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "75ch",
+      variant: "outlined",
+    },
+  },
+}));
+// didnt create with a fragment but unsure
+function createfields() {
+  return (
+    <TextField
+      id="outlined-basic"
+      label="Enter Ingrediant"
+      variant="outlined"
+      placeholder="Carrot"
+    />
+  );
+}
+export default function MultilineTextFields() {
+  const classes = useStyles();
+  return (
+    <Grid container direction="column" alignItems="center">
+      <form className={classes.root} noValidate autoComplete="off">
+        <Grid item>{createfields()}</Grid>
+        <Grid item>{createfields()}</Grid>
+        <Grid item>{createfields()}</Grid>
+        <Grid item>
+          <Grid
+            container
+            direction="column"
+            alignItems="flex-end"
+            justify="flex-end"
+          >
+            <Grid item>
+              <IconButton aria-label="add">
+                <AddIcon />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <Button variant="contained">Submit</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </Grid>
+  );
+}
