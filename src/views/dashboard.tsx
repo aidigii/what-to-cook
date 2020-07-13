1fc5e8f155e61d20e0d4f062ea4d0cc16802f1d8
Aliah <asndeguzman@gmail.com>
2020-07-13 15:59:03 -0500

    render all saved recipes

--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -26,7 +26,8 @@ import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
 import User from "./user";
-import Ingrediants from "./Ingredients";
+import Ingredients from "./Ingredients";
+import Bookmark from './bookmark';
 
 const drawerWidth = 240;
 
@@ -138,9 +139,9 @@ export default function Dashboard() {
     if (display == "user") {
       return <User />;
     } else if (display == "saved") {
-      console.log("saved");
+      return <Bookmark />
     } else if (display == "find") {
-      return <Ingrediants />;
+      return <Ingredients />;
     } else {
       console.log("other things");
     }
